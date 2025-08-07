9b67943c7cc3dd349febebcbd94ccdc8b4a10a9c
A-Random-Username-4m3 <177079733+A-Random-Username-4m3@users.noreply.github.com>
2025-08-08 04:13:31 +0800

    myprofile page start

---
+++ scripts/myprofile.js
@@ -0,0 +1,12 @@
+
+
+function main() {
+	let username = getNameOfLoggedInUser()
+	if (username !== null) { 
+		document.getElementById("pageTitle").textContent = username + "'s account"
+		
+	} else { // handle non logged ins by kicking them straight back to homepage
+		alert("You are forbidden to access this page.")
+		window.location.href = "home.html";
+	}
+} window.addEventListener("load", main);
