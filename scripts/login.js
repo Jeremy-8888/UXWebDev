38d85b7d3aacae94e0598a9c6797cab0bc7fa362
A-Random-Username-4m3 <177079733+A-Random-Username-4m3@users.noreply.github.com>
2025-08-08 04:49:14 +0800

    more stuff to myprofile, split up login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,5 @@
 // requires scripts/backend-sim/account.js
+// requires scripts/validateaccountform.js
 
 function openSignup() {
 	if (new URL(window.location.href).searchParams.get("signup") !== null) {
@@ -13,61 +14,6 @@ function openSignup() {
 function setPageVisible() {
 	document.getElementById("loadingscreen").style.display = "none";
 	document.getElementById("maincontent").style.display = "inline";
-}
-
-function checkRetypedPassword() {
-	const NEWPASSVAL = document.getElementById("newpassword").value;
-	const PASSRET = document.getElementById("passwordretype");
-
-	PASSRET.setCustomValidity("");
-
-	if (NEWPASSVAL !== PASSRET.value) {
-		PASSRET.setCustomValidity("Ensure that the retyped passwords match.");
-	}
-}
-
-function checkPasswordStrength() {
-	const NEWPASS = document.getElementById("newpassword");
-	const NEWPASSVAL = NEWPASS.value;
-
-	NEWPASS.setCustomValidity("");
-	
-	if (NEWPASSVAL.length <= 8) {
-		NEWPASS.setCustomValidity("Increase password length to more than 8 characters!");
-		return;
-	}
-
-	if (!/[0-9]/.test(NEWPASSVAL)) {
-		NEWPASS.setCustomValidity("Add numbers to your password!");
-		return;
-	}
-
-	if (!/[a-z]/.test(NEWPASSVAL)) {
-		NEWPASS.setCustomValidity("Add small letters to your password!");
-		return;
-	}
-
-	if (!/[A-Z]/.test(NEWPASSVAL)) {
-		NEWPASS.setCustomValidity("Add capital letters to your password!");
-		return;
-	}
-
-	if (!/[^A-Za-z0-9]/.test(NEWPASSVAL)) {
-		NEWPASS.setCustomValidity("Add special symbols to your password!");
-		return;
-	}
-}
-
-function checkBirthdayAge() {
-	const AGE = document.getElementById("age");
-	const BDAY = new Date(document.getElementById("bday").value);
-
-	AGE.setCustomValidity("");
-	
-	let yearsSince = Math.floor((new Date() - BDAY) / 31536000000); // 1y to ms
-
-	if (AGE.value != yearsSince)
-		AGE.setCustomValidity("Age does not match birthday.");
 }
 
 function loadCreateAccFormData(signInForm) {
