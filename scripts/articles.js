83bc7072e41afbbe524a6b901a060109a9203059
A-Random-Username-4m3 <177079733+A-Random-Username-4m3@users.noreply.github.com>
2025-08-09 21:32:23 +0800

    font size accesibility

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,4 +1,16 @@
+function setArticleFontSize(toSize) {
+	const MAIN = document.getElementsByTagName("main").item(0);
+	MAIN.style.fontSize = toSize;
+}
 
 function main() {
-
+	document.getElementById("selectsmall").addEventListener(
+		"change", function() {setArticleFontSize("small");}
+	);
+	document.getElementById("selectmedium").addEventListener(
+		"change", function() {setArticleFontSize("medium");}
+	);
+	document.getElementById("selectlarge").addEventListener(
+		"change", function() {setArticleFontSize("large");}
+	);
 } window.addEventListener("load", main);
